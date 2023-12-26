e70fa64b1f23defc8c242c60b233cc28a719d9da
Utkarsh Mandape <utmandape4@gmail.com>
2023-12-26 17:48:53 +0530

    Feat: Add init command

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  cmd "github.com/UtkarshM-hub/Lit/internal/application/commands"
+)
+
+func main() {
+  cmd.Execute()
+}
---
+++ internal/application/commands/root.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "lit",
+	Short: "VCS built in golang",
+	Long: `A Fast and Flexible Static Site Generator built with
+				  love by spf13 and friends in Go.
+				  Complete documentation is available at http://hugo.spf13.com`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+		fmt.Println(args)
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Go-CLI-Tool.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
