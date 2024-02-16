a1e903396ac2c214717571986ea5bc2fda64bf6e
Utkarsh Mandape <utmandape4@gmail.com>
2024-02-16 18:42:37 +0530

    Feat: change active branch

--- internal/application/core/branch.go
+++ internal/application/core/branch.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -20,5 +21,17 @@ func CreateBranch(pathToLit,branchname string) error {
 		return err
 	}
 	defer file.Close()
+	return nil
+}
+
+func ChangeActiveBranch(pathToLit,branchname string) error {
+
+	HEAD_file_path:=filepath.Join(pathToLit,"/.lit/HEAD")
+
+	// change the file content
+	err:=os.WriteFile(HEAD_file_path,[]byte(fmt.Sprintf("ref: /refs/heads/%v",branchname)),0644)
+	if err!=nil{
+		return err
+	}
 	return nil
 }
